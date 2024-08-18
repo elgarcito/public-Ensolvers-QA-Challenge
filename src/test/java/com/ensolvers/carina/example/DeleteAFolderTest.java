8f9357cea0a51e46d9bb8cfc199f9708fe1ef859
elgaricito <edgarjaguirre@hotmail.com>
2024-08-18 11:20:14 -0300

    Delete a folder test created

--- src/test/java/com/ensolvers/carina/example/DeleteAFolderTest.java
+++ src/test/java/com/ensolvers/carina/example/DeleteAFolderTest.java
@@ -1,16 +1,18 @@
 package com.ensolvers.carina.example;
 
-import com.ensolvers.carina.example.gui.pages.common.*;
+import com.ensolvers.carina.example.gui.pages.common.FolderPageBase;
+import com.ensolvers.carina.example.gui.pages.common.HomePageBase;
+import com.ensolvers.carina.example.gui.pages.common.LoginPageBase;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
 import com.zebrunner.carina.utils.R;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class EditAFolderTest implements IAbstractTest {
+public class DeleteAFolderTest implements IAbstractTest {
     @MethodOwner(owner = "edgar")
-    @Test(description = "Edit a previously created folder")
-    public void editAFolderTest(){
+    @Test(description = "delete a previously created folder")
+    public void deleteAFolderTest(){
         LoginPageBase loginPage=initPage(getDriver(), LoginPageBase.class);
         loginPage.open();
         Assert.assertTrue(loginPage.isPageOpened(),"Home page is not opened");
@@ -21,11 +23,10 @@ public class EditAFolderTest implements IAbstractTest {
         homePage.clickManageListsButton();
         FolderPageBase folderPage= homePage.clickFolderButton();
         Assert.assertTrue(folderPage.isPageOpened(),"Folder page is not opened");
-        CreateOrEditAFolderPageBase createOrEditAFolderEditPage= folderPage.clickEditFolderButton();
-        Assert.assertTrue(createOrEditAFolderEditPage.isPageOpened(),"Create or edit a folder page is not opened");
-        createOrEditAFolderEditPage.typeFolderName(R.TESTDATA.get("newFolderName"));
-        createOrEditAFolderEditPage.clickSaveButton();
-        Assert.assertTrue(folderPage.checkUpdatedMessageAppear(),"Updated folder message not appeared");
+        folderPage.clickDeleteFolderButton();
+        Assert.assertTrue(folderPage.checkConfirmDeleteOperationModalAppear(),"Confirm delete operation modal did not appeared");
+        folderPage.clickConfirmDeleteFolderButton();
+        Assert.assertTrue(folderPage.checkDeletedMessageAppear(),"Deleted folder message not appeared");
 
     }
 }
